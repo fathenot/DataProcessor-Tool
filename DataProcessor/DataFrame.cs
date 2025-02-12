e99f3f27d7260e77eda7bc8df1f873b8dff09ad5
Trần Quốc Cường <l910tranquoccuong2019@gmail.com>
2025-02-12 20:47:55 +0700

    update the dataFrame class and fix bug of the Series with generic types

--- DataProcessor/DataFrame.cs
+++ DataProcessor/DataFrame.cs
@@ -6,30 +6,72 @@ using System.Threading;
 using System.Buffers;
 using System.Data.Common;
 using System.Data.SqlTypes;
+using System.Data;
+using System.ComponentModel.DataAnnotations;
 namespace DataProcessor
 {
 
     public class DataFrame
     {
         private List<Series> table;
-
+        private List<String> columns;
+        private int numRows;
         public DataFrame(List<Series> table)
         {
-            this.table = table;
+            if(table == null) throw new ArgumentNullException("table must not be null");
+            this.table = new List<Series>(table);
+            this.columns = new List<String>();
+            numRows = 0;
+            foreach(Series s in table)
+            {
+                this.table.Add(new Series(s));
+                this.columns.Add(s.Name);
+                numRows = Math.Max(0, s.Count);
+            }
         }
 
-        public DataFrame(Dictionary<string, IList<object>> items)// string for column name, List<string> for value in a column
+        public DataFrame(Dictionary<string, IList<object>> items)// string for column name, List<string> for values in a column
         {
             if (items == null)
             {
                 throw new ArgumentNullException($"{nameof(items)} must not be null");
             }
             table = new List<Series>();
+            columns = new List<String>();
             foreach (var columnName in items.Keys)
             {
                 Series column = new Series(columnName, items[columnName]);
                 table.Add(column);
+                columns.Add(columnName);
             }
+        }
+        public DataFrame(DataFrame other)
+        {
+            this.table = new List<Series>(other.table);
+            this.columns = new List<String>(other.Columns);
+        }
+        public List<String> Columns => columns;
+        public void Describe() { throw new NotImplementedException("this method is under construction"); }
+        public DataFrame Head(int numberRows = 5)
+        {
+            if(numberRows < 0) { throw new ArgumentOutOfRangeException("number of rows must not be negative"); }
+            List<Series> list = new List<Series>();
+            foreach (Series s in this.table)
+            {
+                list.Add(new Series(s.Name, s.Values is List<object> listRef ? listRef.GetRange(0, Math.Min(numberRows, listRef.Count)) : s.Values.Take(numberRows).ToList()));
+            }
+            return new DataFrame(list);
+        }
+        public DataFrame Tail(int numberRows = 5)
+        {
+            if (numberRows < 0) { throw new ArgumentOutOfRangeException("number of rows must not be negative"); }
+            List<Series> list = new List<Series>();
+            foreach (Series s in this.table)
+            {
+                list.Add(new Series(s.Name, s.Values is List<object> listRef ? listRef.GetRange(table[0].Count - numberRows, numberRows) : s.Values.Take(numberRows).ToList()));
+            }
+            return new DataFrame(list);
+
         }
     }
 }
--- DataProcessor/Series.cs
+++ DataProcessor/Series.cs
@@ -32,7 +32,7 @@ namespace DataProcessor
         }
 
         // utility
-        public object Name { get { return this.name; } }
+        public String Name { get { return this.name; } }
         public IList<object> Values { get { return this.values; } }
         public int Count => values.Count;
         public bool IsReadOnly { get { return false; } }
@@ -171,6 +171,180 @@ namespace DataProcessor
             return Indexes;
         }
         public static void print(Series series)
+        {
+            Console.WriteLine(series.Name);
+            int rowIndex = 1;
+            foreach (var item in series.Values)
+            {
+                Console.WriteLine($"{rowIndex} {item}");
+            }
+        }
+    }
+
+
+    public class Series<DataType> : ICollection<DataType>
+    {
+        private string name;
+        private IList<DataType> values;
+
+        public Series(string name, IList<DataType> values)
+        {
+            this.name = name;
+            this.values = values;
+        }
+
+        public Series(Series <DataType> other)
+        {
+            this.name = other.name;
+            this.values = new List<DataType>(other.Values);
+        }
+
+        public Series(Tuple<string, IList<DataType>> KeyAndValues)
+        {
+            this.name = KeyAndValues.Item1;
+            this.values = new List<DataType>(KeyAndValues.Item2);
+        }
+
+        // utility
+        public String Name { get { return this.name; } }
+        public IReadOnlyList<DataType> Values => values as IReadOnlyList<DataType> ?? values.ToList();
+        public int Count => values.Count;
+        public bool IsReadOnly => false;
+        public object this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= values.Count)
+                    throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range");
+                return values[index];
+            }
+            set
+            {
+                if (index < 0 || index >= values.Count)
+                    throw new ArgumentOutOfRangeException(nameof(index), "Index is out of range");
+                values[index] = (DataType)value;
+            }
+        }
+        // iterator
+        public IEnumerator<DataType> GetEnumerator()
+        {
+            return values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return values.GetEnumerator();
+        }
+
+        //method
+        public void Add (DataType item)
+        {
+            values.Add(item);
+        }
+        public bool Remove(DataType item)
+        {
+            return values.Remove(item);
+        }
+        public void Clear()
+        {
+            values.Clear();
+        }
+        public bool Contains(DataType item)
+        {
+            return values.Contains(item);
+        }
+        public void CopyTo(DataType[] array, int arrayIndex)
+        {
+            values.CopyTo((DataType[])array, arrayIndex);
+        }
+
+        public IList<DataType> getItem(int indexFrom, int indexTo, int step = 1)
+        {
+            IList<DataType> result = new List<DataType>();
+
+            // check valid argument
+            if (indexFrom < 0 || indexTo < 0 || indexFrom >= values.Count || indexTo >= values.Count)
+            {
+                throw new ArgumentOutOfRangeException("Index is out of range");
+            }
+
+            if (step < 0)
+            {
+                if (indexFrom < indexTo)
+                {
+                    throw new ArgumentException("Index from must be greater than index to when step < 0");
+                }
+            }
+
+            if (step > 0 && indexFrom > indexTo)
+            {
+                throw new ArgumentException("Index from must be smaller than index to when step > 0");
+            }
+
+            if (step > values.Count)
+            {
+                throw new ArgumentOutOfRangeException("Step greater than size of series");
+            }
+
+            if (step == 0)
+            {
+                throw new ArgumentException("Step must not be zero");
+            }
+
+            //main logic of the method
+            if (indexTo == indexFrom)
+            {
+                return result;  // Trả về danh sách rỗng theo phong cách Python
+            }
+            for (int i = indexFrom; i != indexTo; i += step)
+            {
+                result.Add(values[i]);
+            }
+            return result;
+        }
+
+        public void Sort(Comparer<DataType>? comparer = null)
+        {
+            if (comparer == null)
+            {
+                // Sử dụng OrderBy với logic null được đưa về cuối
+                values = values.OrderBy(x => x != null)
+                               .ThenBy(x => x as IComparable)  // Đảm bảo sắp xếp bình thường sau khi xử lý null
+                               .Concat(values.Where(x => x == null))
+                               .ToList();
+            }
+            else
+            {
+                // Sử dụng OrderBy với comparer, với logic null được đưa về cuối
+                values = values.OrderBy(x => x == null ? 1 : 0)
+                               .ThenBy(x => x, comparer)  // Sắp xếp với comparer nếu có
+                               .ToList();
+            }
+        }
+
+        public void changeItem(int index, DataType item)
+        {
+            if (index < 0 || index >= values.Count)
+            {
+                throw new ArgumentOutOfRangeException("index is out of range");
+            }
+            this.values[index] = item;
+        }
+        // searching and filter
+        public IList<DataType> Filter(Func<DataType, bool> filter)
+        {
+            return values.AsEnumerable().Where(filter).ToList();
+        }
+        public IList<int> Find(DataType? item)
+        {
+            IList<int> Indexes = new List<int>();
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (Equals(this[i], item)) { Indexes.Add(i); }
+            }
+            return Indexes;
+        }
+        public static void print(Series<DataType> series)
         {
             Console.WriteLine(series.Name);
             int rowIndex = 1;
