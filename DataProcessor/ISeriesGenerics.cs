64e28ebf89437b2ee0a17890f3ff88457af0a19b
Trần Quốc Cường <l910tranquoccuong2019@gmail.com>
2025-02-27 11:20:56 +0700

    add series generics interface

---
+++ DataProcessor/ISeriesGenerics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessor
+{
+    public interface ISeries<DataType> : IEnumerable<DataType>
+    {
+        string? Name { get; }
+        IReadOnlyList<DataType> Values { get; }
+        Type dType { get; }
+        bool IsReadOnly { get; }
+        void Clear();
+        bool Remove(DataType item);
+        void Add(DataType item);
+        int Count { get; }
+        ISeries<DataType> Clone();
+        IList<int> Find(DataType item);
+        DataType this[int index] { get; set; }
+
+        // default IEnumerable<DataType>
+        IEnumerator<DataType> IEnumerable<DataType>.GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        // default IEnumerable (non-generic)
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable<DataType>)this).GetEnumerator();
+    }
+}
