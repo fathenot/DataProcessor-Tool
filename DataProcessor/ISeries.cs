9373f2e41aefb20bd2ef00e7a98c76d9c7b0e70b
Trần Quốc Cường <l910tranquoccuong2019@gmail.com>
2025-03-02 22:25:16 +0700

    move interface from series.cs to this file

---
+++ DataProcessor/ISeries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessor
+{
+    public interface ISeries
+    {
+        public string? Name { get; }
+        public IReadOnlyList<object?> Values { get; }
+        public Type dType { get; }
+        public bool IsReadOnly { get; }
+        public void Clear();
+        public bool Remove(object item);
+        public void Add(object item);
+        public bool IsValidType(object? value);
+        public int Count { get; }
+        public ISeries Clone();
+        public ISeries AsType(Type NewType, bool ForceCast = false);
+        public IList<int> Find(object? item);
+        public object? this[int index] { get; set; }
+        public ISeries View(ValueTuple<int, int, int> slice);
+    }
+}
