3b57e3fec340476523fe02968fd5cd0035b52a64
Trần Quốc Cường <l910tranquoccuong2019@gmail.com>
2025-03-09 23:19:58 +0700

    remove this[int index] properties and IEumerator default implementation

--- DataProcessor/ISeriesGenerics.cs
+++ DataProcessor/ISeriesGenerics.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DataProcessor
 {
-    public interface ISeries<DataType>  : IEnumerable<DataType> where DataType : notnull
+    public interface ISeries<DataType> where DataType : notnull
     {
         string? Name { get; }
         IReadOnlyList<DataType> Values { get; }
@@ -18,18 +18,5 @@ namespace DataProcessor
         void Add(DataType item);
         int Count { get; }
         IList<int> Find(DataType item);
-        DataType this[int index] { get; set; }
-
-        // default IEnumerable<DataType>
-        IEnumerator<DataType> IEnumerable<DataType>.GetEnumerator()
-        {
-            for (int i = 0; i < Count; i++)
-            {
-                yield return this[i];
-            }
-        }
-
-        // default IEnumerable (non-generic)
-        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable<DataType>)this).GetEnumerator();
     }
 }
