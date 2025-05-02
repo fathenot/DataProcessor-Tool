9f501dd1151ba9d39cb6433c8f49b9b1880ce1a1
Trần Quốc Cường <l910tranquoccuong2019@gmail.com>
2025-05-02 23:47:08 +0700

    change file name

---
+++ DataProcessor/source/ValueStorage/DoubleValueStorage.cs
@@ -0,0 +1,53 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DataProcessor.source.ValueStorage
+{
+    internal class DoubleValueStorage : ValueStorage
+    {
+        private readonly double?[] array;
+        GCHandle handle;
+
+        internal DoubleValueStorage(double?[] array)
+        {
+            this.array = array;
+            handle = GCHandle.Alloc(array, GCHandleType.Pinned);
+        }
+
+        public override nint GetArrayAddress()
+        {
+            return handle.AddrOfPinnedObject();
+        }
+        public override object? GetValue(int index)
+        {
+            return array[index];
+        }
+        public override int Length => array.Length;
+        public override IEnumerable<int> NullPositions
+        {
+            get
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] == null)
+                    {
+                        yield return i;
+                    }
+                }
+            }
+        }
+
+        public override void SetValue(int index, object? value)
+        {
+            if (value is double doubleValue)
+            {
+               array[index] = (double)value;
+            }
+            if (value is null)
+            {
+                array[index] = null;
+            }
+        }
+
+        public override Type ValueType => typeof(double);
+    }
+}
