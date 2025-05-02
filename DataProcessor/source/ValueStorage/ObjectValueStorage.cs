c22a4ae10a1e2c5a08799d9ae087f1e659bd985b
Trần Quốc Cường <l910tranquoccuong2019@gmail.com>
2025-05-03 00:03:18 +0700

    add object and string values storage

---
+++ DataProcessor/source/ValueStorage/ObjectValueStorage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessor.source.ValueStorage
+{
+    internal class ObjectValueStorage: ValueStorage
+    {
+        private readonly object?[] objects;
+        GCHandle handle;
+
+        internal ObjectValueStorage(object?[] objects)
+        {
+            this.objects = objects;
+            handle = GCHandle.Alloc(objects, GCHandleType.Pinned);
+        }
+
+        public override nint GetArrayAddress()
+        {
+            return handle.AddrOfPinnedObject();
+        }
+
+        public override object? GetValue(int index)
+        {
+            return objects[index];
+        }
+
+        public override void SetValue(int index, object? value)
+        {
+            objects[index] = value;
+        }
+
+        public override int Length => objects.Length;
+        public override IEnumerable<int> NullPositions
+        {
+            get
+            {
+                for (int i = 0; i < objects.Length; i++)
+                {
+                    if (objects[i] == null)
+                    {
+                        yield return i;
+                    }
+                }
+            }
+        }
+
+        public override Type ValueType => typeof(object);
+    }
+}
---
+++ DataProcessor/source/ValueStorage/StringValueStorage.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DataProcessor.source.ValueStorage
+{
+    internal class StringStorage : ValueStorage
+    {
+        private readonly string?[] strings;
+        GCHandle handle;
+
+        internal StringStorage(string?[] strings)
+        {
+            this.strings = strings;
+            handle = GCHandle.Alloc(strings, GCHandleType.Pinned);
+        }
+
+        public string?[] Strings
+        {
+            get { return strings; }
+        }
+        public GCHandle Handle { get { return handle; } }
+
+        public override nint GetArrayAddress()
+        {
+           return handle.AddrOfPinnedObject();
+        }
+        public override object? GetValue(int index)
+        {
+            return strings[index];
+        }
+        public override void SetValue(int index, object? value)
+        {
+            if(value == null)
+            {
+                strings[index] = null;
+            }
+            if(value is string stringValue)
+            {
+                strings[index] = stringValue;
+            }
+        }
+
+        public override int Length => strings.Length;
+
+        public override IEnumerable<int> NullPositions
+        {
+            get
+            {
+                for (int i = 0; i < strings.Length; i++)
+                {
+                    if (strings[i] == null)
+                    {
+                        yield return i;
+
+                    }
+                }
+            }
+        }
+
+        public override Type ValueType => typeof(string);
+    }
+}
