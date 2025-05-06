ee5d64645d4a2b304ee9ea11b0054e1ad951da21
Trần Quốc Cường <l910tranquoccuong2019@gmail.com>
2025-05-07 01:12:47 +0700

    integrate null bitmap

--- DataProcessor/source/ValueStorage/IntValuesStorage.cs
+++ DataProcessor/source/ValueStorage/IntValuesStorage.cs
@@ -4,18 +4,30 @@ namespace DataProcessor.source.ValueStorage
 {
     internal class IntStorage : ValueStorage
     {
-        private readonly long?[] array;
+        private readonly long[] array;
+        NullBitMap bitMap;
         GCHandle handle;
 
         internal IntStorage(long?[] intValues)
         {
-            this.array = intValues;
-            handle = GCHandle.Alloc(array, GCHandleType.Pinned | GCHandleType.Pinned);
+            array = new long[intValues.Length];
+            bitMap = new NullBitMap(intValues.Length);
+
+            for (int i = 0; i < intValues.Length; i++)
+            {
+                bitMap.SetNull(i, intValues[i] == null);
+
+                if (intValues[i] != null)
+                {
+                    array[i] = (long)intValues[i];
+                }
+            }
+            handle = GCHandle.Alloc(array, GCHandleType.Pinned);
         }
 
         public override object? GetValue(int index)
         {
-            return array.GetValue(index);
+            return bitMap.IsNull(index) ? null : array[index];
         }
 
         public override nint GetArrayAddress()
@@ -24,7 +36,8 @@ namespace DataProcessor.source.ValueStorage
         }
 
         public override int Length => array.Length;
-        public override Type ValueType => typeof(int);
+        public override Type ValueType => typeof(long);
+        public int CountNullValues => bitMap.CountNulls();
 
         public override IEnumerable<int> NullPositions
         {
@@ -32,7 +45,7 @@ namespace DataProcessor.source.ValueStorage
             {
                 for (int i = 0; i < array.Length; i++)
                 {
-                    if (array[i] == null)
+                    if (bitMap.IsNull(i))
                     {
                         yield return i;
                     }
@@ -44,13 +57,19 @@ namespace DataProcessor.source.ValueStorage
         public override void SetValue(int index, object? value)
         {
             if (index < 0 || index >= array.Length) throw new ArgumentOutOfRangeException(nameof(index));
-            if (value is int intValue)
+            if (value is long longValue)
             {
-                array.SetValue(index, intValue);
+                array[index] = longValue;
+                bitMap.SetNull(index, false);
             }
             if (value is null)
             {
-                array[index] = null;
+                bitMap.SetNull(index, true);
+                array[index] = default;
+            }
+            else
+            {
+                throw new InvalidCastException("Expected a long or null");
             }
         }
         ~IntStorage()
