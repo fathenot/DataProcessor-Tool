8e3ba8867cc143876ec74fcc976cf1908e9c3e0b
fathenot <L910tranquoccuong2019@gmail.com>
2025-05-20 01:59:40 +0700

    update multikey index

--- DataProcessor/source/Index/MultiIndex.cs
+++ DataProcessor/source/Index/MultiIndex.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataProcessor.source.Index
+﻿namespace DataProcessor.source.Index
 {
     public readonly struct MultiKey : IEquatable<MultiKey>
     {
@@ -15,6 +9,15 @@ namespace DataProcessor.source.Index
             _values = values ?? throw new ArgumentNullException(nameof(values));
         }
 
+
+        public MultiKey(MultiKey other)
+        {
+            _values = new object[other.Values.Length];
+            for (int i = 0; i < other.Values.Length; i++)
+            {
+                _values[i] = other.Values[i];
+            }
+        }
         // Sửa lại Equals để kiểm tra null đúng cách và so sánh theo giá trị từng phần tử
         public bool Equals(MultiKey other)
         {
@@ -55,19 +58,21 @@ namespace DataProcessor.source.Index
 
     public class MultiIndex : IIndex
     {
-        private new List<object[]> indexList;  // mỗi object[] là một tuple index
-        private new Dictionary<MultiKey, List<int>> indexMap;
+        private List<MultiKey> indexList;
+        private Dictionary<MultiKey, List<int>> indexMap;
 
 
         public MultiIndex(List<object[]> indexList) : base()
         {
-            this.indexList = indexList;
+            this.indexList = new List<MultiKey>(indexList.Count);
             indexMap = new Dictionary<MultiKey, List<int>>();
 
             // Xây dựng dictionary ánh xạ giữa index và các vị trí
             for (int i = 0; i < indexList.Count; i++)
             {
                 var key = new MultiKey(indexList[i]);
+                this.indexList[i] = key;
+
                 if (!indexMap.ContainsKey(key))
                 {
                     indexMap[key] = new List<int>();
@@ -75,6 +80,28 @@ namespace DataProcessor.source.Index
                 indexMap[key].Add(i);
             }
         }
+
+        public MultiIndex(List<MultiKey> indexList)
+        {
+            this.indexList = indexList;
+            this.indexMap = new Dictionary<MultiKey, List<int>>();
+            for (int i = 0; i < indexList.Count; i++)
+            {
+                var key = indexList[i];
+                if (!indexMap.ContainsKey(key))
+                {
+                    indexMap[key] = new List<int>();
+                }
+                indexMap[key].Add(i);
+            }
+        }
+
+        // properties
+        public override int Count => indexList.Count;
+
+        public override IReadOnlyList<object> IndexList => this.indexList.Cast<object>().ToList().AsReadOnly();
+
+        // methods
         public override MultiIndex Slice(int start, int end, int step = 1)
         {
             // Cắt phần tử từ indexList theo start, end, step
@@ -98,6 +125,21 @@ namespace DataProcessor.source.Index
             var multiKey = new MultiKey(key);
             return indexMap.ContainsKey(multiKey);
         }
+
+        public override bool Contains(object key)
+        {
+            if (key is MultiKey multiKey)
+            {
+                return indexMap.ContainsKey(multiKey);
+            }
+
+            if(key is object[] arr)
+            {
+                return this.Contains(arr);
+            }
+
+            throw new ArgumentException($"{nameof(key)} must be object array or multikey");
+        }
         public IReadOnlyList<int> GetIndexPosition(object[] key)
         {
             var multiKey = new MultiKey(key);
@@ -108,6 +150,33 @@ namespace DataProcessor.source.Index
             throw new KeyNotFoundException($"Key {string.Join(", ", key)} not found.");
         }
 
+        public override IReadOnlyList<int> GetIndexPosition(object index)
+        {
+            if (index is object[] arr)
+            {
+                return GetIndexPosition(arr);
+            }
+            if (index is MultiKey multiKey)
+            {
+                return indexMap[multiKey];
+            }
+            throw new ArgumentException($"index {nameof(index)} must be array of object of multikey");
+        }
+
+        public override int FirstPositionOf(object key)
+        {
+            if(key is MultiKey multiKey)
+            {
+                return indexList.IndexOf(multiKey);
+            }
+            if( key is object[] arr)
+            {
+                var tmp = new MultiKey(arr);
+                return indexList.IndexOf(tmp);
+            }
+            throw new ArgumentException($"{nameof(key)} must be object array or multikey");
+        }
+
         // Phương thức để lấy index của tuple tại vị trí idx
         public override object GetIndex(int idx)
         {
@@ -115,9 +184,17 @@ namespace DataProcessor.source.Index
         }
 
         // Phương thức để lấy các distinct index
-        public new IEnumerable<object[]> DistinctIndices()
+        public override IEnumerable<object> DistinctIndices()
         {
-            return indexMap.Keys.Select(k => k.Values);
+            return indexList.Distinct().Cast<object>();
+        }
+
+        public override IEnumerator<object> GetEnumerator()
+        {
+            for (int i = 0; i < indexList.Count; i++)
+            {
+                yield return indexList[i];
+            }
         }
     }
 }
