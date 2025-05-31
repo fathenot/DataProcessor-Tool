57997ad1c1307d28f072a83310a036ddf8468cfc
fathenot <L910tranquoccuong2019@gmail.com>
2025-05-31 23:37:55 +0700

    add char storage and decimal storage

---
+++ DataProcessor/source/ValueStorage/CharStorage.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace DataProcessor.source.ValueStorage
+{
+    internal class CharStorage : ValueStorage
+    {
+        char[] chars;
+        NullBitMap nullbitMap;
+        GCHandle handle;
+
+        public CharStorage(char?[] chars)
+        {
+            this.chars = new char[chars.Length];
+            nullbitMap = new NullBitMap(chars.Length);
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == null)
+                {
+                    nullbitMap.SetNull(i, true);
+                    this.chars[i] = default;
+                }
+                else
+                {
+                    this.chars[i] = Convert.ToChar(chars[i]);
+                    nullbitMap.SetNull(i, false);
+                }
+            }
+            handle = GCHandle.Alloc(this.chars, GCHandleType.Pinned);
+        }
+
+        internal override Type ElementType => typeof(char);
+
+        internal override int Count => chars.Length;
+
+        internal override nint GetNativeBufferPointer()
+        {
+            return handle.AddrOfPinnedObject();
+        }
+
+        internal override object? GetValue(int index)
+        {
+            if(nullbitMap.IsNull(index))
+            {
+                return null;
+            }
+            return chars[index];
+        }
+
+        internal override IEnumerable<int> NullIndices
+        {
+            get
+            {
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    if (nullbitMap.IsNull(i))
+                    {
+                        yield return i;
+                    }
+                }
+            }
+        }
+
+        internal override void SetValue(int index, object? value)
+        {
+            if (value is char charValue)
+            {
+                chars[index] = charValue;
+                nullbitMap.SetNull(index, false);
+            }
+            else if (value is null)
+            {
+                chars[index] = default;
+                nullbitMap.SetNull(index, true);
+            }
+            else
+            {
+                throw new InvalidCastException($"Expected a value of type {typeof(char)} or null.");
+            }
+
+        }
+
+        ~CharStorage()
+        {
+            if (handle.IsAllocated)
+            {
+                handle.Free();
+            }
+        }
+    }
+}
---
+++ DataProcessor/source/ValueStorage/DecimalStorage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace DataProcessor.source.ValueStorage
+{
+    internal class DecimalStorage : ValueStorage
+    {
+        private readonly decimal[] decimals;
+        NullBitMap nullBitMap;
+        GCHandle handle;
+        internal DecimalStorage(decimal?[] decimals)
+        {
+            this.decimals = new decimal[decimals.Length];
+            nullBitMap = new NullBitMap(decimals.Length);
+            handle = GCHandle.Alloc(decimals, GCHandleType.Pinned);
+            for (int i = 0; i < decimals.Length; i++)
+            {
+                if (decimals[i] == null)
+                {
+                    nullBitMap.SetNull(i, true);
+                    this.decimals[i] = default; // Set to default value for decimal
+                }
+                else
+                {
+                    this.decimals[i] = decimals[i].Value;
+                    nullBitMap.SetNull(i, false);
+                }
+            }
+        }
+        internal override nint GetNativeBufferPointer()
+        {
+            return handle.AddrOfPinnedObject();
+        }
+        internal override object? GetValue(int index)
+        {
+            return nullBitMap.IsNull(index) ? null : decimals[index];
+        }
+        internal override void SetValue(int index, object? value)
+        {
+            if (value is decimal decimalValue)
+            {
+                decimals[index] = decimalValue;
+            }
+            else
+            {
+                nullBitMap.SetNull(index, true);
+                decimals[index] = default; // Reset to default value if null
+            }
+        }
+        internal override int Count => decimals.Length;
+        internal override IEnumerable<int> NullIndices
+        {
+            get
+            {
+                for (int i = 0; i < decimals.Length; i++)
+                {
+                    if (nullBitMap.IsNull(i))
+                    {
+                        yield return i;
+                    }
+                }
+            }
+        }
+        internal override Type ElementType => typeof(decimal);
+    }
+}
